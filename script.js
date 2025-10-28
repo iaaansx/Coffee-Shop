b00da6532da62db7d0dbd08d16f0e51711303e45
Tetzuuu <reymart0706@gmail.com>
2025-10-28 10:29:11 +0800

    git push

--- script.js
+++ script.js
@@ -11,3 +11,69 @@ signIn.addEventListener('click', (e) => {
     e.preventDefault();
     logIn.classList.remove('active');
 });
+
+// Sign Up Form Handling
+
+const signinForm = document.querySelector('.signin-form');
+const signupForm = document.querySelector('.signup-form');
+
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const username = document.querySelector('input[type="text"]').value.trim();
+    const password = document.querySelector('input[type="password"]').value.trim();
+    const email = document.querySelector('input[type="email"]').value.trim();
+
+    if(username && password && email) {
+        console.log('Sign Up Successful'); 
+    }
+
+    let users = JSON.parse(localStorage.getItem('users')) || [];
+
+
+    if(users.some((user) => user.username === username)) {
+        alert('Username already exists. Please choose a different one.');
+        return;
+    }
+
+    users.push({ username, password, email });
+    localStorage.setItem('users', JSON.stringify(users));
+    alert('Sign Up Successful! You can now sign in.');
+    signupForm.reset();
+    logIn.classList.remove('active');
+});
+
+// Sign In Form Handling
+
+signinForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const username = document.querySelector('.signin-form input[type="text"]').value.trim();
+    const password = document.querySelector('.signin-form input[type="password"]').value.trim();
+    const rememberMe = document.querySelector('.signin-form input[type="checkbox"]').checked;
+
+    let users = JSON.parse(localStorage.getItem('users')) || [];
+
+    const validUser = users.find((user) => user.username === username && user.password === password);
+
+    if(validUser) {
+        alert('☕ Welcome back,' + ' ' + validUser.username + '!' + ' Enjoy your coffee!');
+
+    if(rememberMe) {    
+        localStorage.setItem('currentUser', JSON.stringify({validUser}));
+    } else {
+        sessionStorage.setItem('currentUser', JSON.stringify({validUser}));
+    }
+
+    window.location.href = 'dashboard.html';
+    } else {
+        alert('Invalid username or password. Please try again.');
+    }
+});
+
+window.addEventListener('domContentLoaded', () => {
+    const currentUser = JSON.parse(localStorage.getItem('currentUser')) || JSON.parse(sessionStorage.getItem('currentUser'));
+    if(currentUser) {
+        window.location.href = 'dashboard.html';
+    }
+});
